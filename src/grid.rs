45833776dac105a0d9a2c3497e9efb887a90c3dc
Wormss9 <Wormss9.bd@gmail.com>
2026-06-02 02:38:11 +0200

    Add triangle and square grids with edges.

---
+++ src/grid.rs
@@ -0,0 +1,96 @@
+pub struct Grid {
+    grid: Vec<Vec<usize>>,
+}
+pub struct Edges {
+    pub edges: Vec<Vec<usize>>,
+}
+
+impl Edges {
+    pub fn square(size: usize) -> Self {
+        let max = size * size;
+
+        let mut edges = vec![Vec::with_capacity(4); max];
+
+        for i in 0..size {
+            for j in 0..size {
+                let id = i + j * size;
+                if j > 0 {
+                    edges[id].push(id - size);
+                }
+                if i > 0 {
+                    edges[id].push(id - 1);
+                }
+                if i + 1 < size {
+                    edges[id].push(id + 1);
+                }
+                if j + 1 < size {
+                    edges[id].push(id + size);
+                }
+            }
+        }
+        Self { edges }
+    }
+    pub fn triangle_hex(size: usize) -> Self {
+        let max = 6 * size * size;
+        let mut edges = vec![Vec::with_capacity(3); max];
+        for y in 0..size * 2 {
+            for x in 0..size * 4 {
+                match get_id(x, y, size) {
+                    Some(current) => {
+                        if x > 0
+                            && let Some(right) = get_id(x - 1, y, size)
+                        {
+                            edges[current].push(right);
+                        }
+                        if x < size * 4 - 1
+                            && let Some(left) = get_id(x + 1, y, size)
+                        {
+                            edges[current].push(left);
+                        }
+                        if x % 2 == 0 {
+                            if y > 0
+                                && let Some(top) =
+                                    get_id(if y < size { x - 1 } else { x + 1 }, y - 1, size)
+                            {
+                                edges[current].push(top);
+                            }
+                        } else {
+                            if y < size * 2
+                                && let Some(bottom) =
+                                    get_id(if y < size { x - 1 } else { x + 1 }, y + 1, size)
+                            {
+                                edges[current].push(bottom);
+                            }
+                        }
+                    }
+                    None => continue,
+                }
+            }
+        }
+        Self { edges }
+    }
+}
+
+fn front_skip(x: usize, y: usize, size: usize) -> bool {
+    size > y && x < 2 * (size - y) - 1
+}
+
+fn back_skip(x: usize, y: usize, size: usize) -> bool {
+    y >= size && x > 6 * size - 2 * y - 2
+}
+
+pub fn get_id(x: usize, y: usize, size: usize) -> Option<usize> {
+    if x >= size * 4 || y >= size * 2 || front_skip(x, y, size) || back_skip(x, y, size) {
+        return None;
+    }
+
+    let raw = y * size * 4 + x;
+
+    let gap = if y < size {
+        (y + 1) * (2 * size - y - 1)
+    } else {
+        size * size + (y - size) * (y - size)
+    };
+
+    Some(raw - gap)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+mod grid;
+
 use bevy::prelude::*;
 
+use grid::Edges;
+
 fn main() {
-   App::new()
-       .add_systems(Update, hello_world_system)
-       .run();
+    //  App::new().add_systems(Update, hello_world_system).run();
+    hello_world_system();
 }
 
 fn hello_world_system() {
-   println!("hello world");
+    let edges = Edges::triangle_hex(1);
+    println!("{:?}", edges.edges);
+    let edges = Edges::triangle_hex(2);
+    println!("{:?}", edges.edges);
+    let edges = Edges::triangle_hex(3);
+    println!("{:?}", edges.edges);
 }
